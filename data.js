ab236a0cc4209f91ee7e946df8d3a6aa3ecb80c0
alancedano2 <supernatura94@yahoo.com>
2025-04-18 18:30:00 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+
+const streams = {
+  netflix1: {
+    title: "Netflix 1",
+    url: "https://tulink.m3u8"
+  },
+  espn: {
+    title: "ESPN",
+    url: "https://tulink.m3u8"
+  },
+  disney: {
+    title: "Disney Channel",
+    url: "https://tulink.m3u8"
+  },
+  hbo1: {
+    title: "HBO Max 1",
+    url: "https://tulink.m3u8"
+  },
+  liga1max: {
+    title: "Liga 1 Max",
+    url: "https://tulink.m3u8"
+  }
+};
